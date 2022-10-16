55209b3312e172d782b030c55baff2e30bf07d01
shota kikuchi <shota3557@gmail.com>
2022-10-16 13:55:16 +0900

    ログアウト機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:sessions][:email].downcase
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password]) #authenticateはパスワードが一致してるか判定するメソッド
-    
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end  
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to new_session_path
   end
 end
