f340732772619bcb9aae66b176c5ef2e0ea58095
shota kikuchi <shota3557@gmail.com>
2022-10-17 14:47:51 +0900

    destroyアクションを定義した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -18,7 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path    
+      redirect_to user_path(@user.id)    
     else
       render :new
     end
@@ -29,7 +30,8 @@ class UsersController < ApplicationController
   end
 
   def destroy
- 
+    @user.destroy
+    redirect_to users_path
   end
 
   private
