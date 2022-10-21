583362d2c275e8b148e51a40bb29373e37e0600a
shota kikuchi <shota3557@gmail.com>
2022-10-21 10:38:07 +0900

    開発環境でのメール送信の際のホスト名を変更した

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   config.active_storage.service = :local
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'fast-reaches-67749.herokuapp.com/' }
   config.action_mailer.delivery_method = :letter_opener_web
   config.active_support.deprecation = :log
   config.active_support.disallowed_deprecation = :raise
