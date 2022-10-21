c7527a46782187a0a730e25c70bb66f350315bd5
shota kikuchi <shota3557@gmail.com>
2022-10-21 16:07:03 +0900

    未入力時のバリデーションを設定した

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates :content, presence: true
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
